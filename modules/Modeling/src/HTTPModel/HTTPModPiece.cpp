eddebd0967787f2323885ed4418d3e75d058485e
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2022-02-24 18:24:46 +0100

    Add support for HTTP protocol with derivative information

--- modules/Modeling/src/HTTPModel/HTTPModPiece.cpp
+++ modules/Modeling/src/HTTPModel/HTTPModPiece.cpp
@@ -23,6 +23,43 @@ Eigen::VectorXi HTTPModPiece::read_output_size(const std::string host, const htt
 }
 
 void HTTPModPiece::EvaluateImpl(muq::Modeling::ref_vector<Eigen::VectorXd> const& inputs) {
+  if (!client.SupportsEvaluate())
+    throw std::runtime_error("Model does not support evaluation!");
   client.Evaluate(inputs, config);
   outputs = client.outputs;
+}
+
+void HTTPModPiece::GradientImpl(unsigned int outWrt,
+                                unsigned int inWrt,
+                                muq::Modeling::ref_vector<Eigen::VectorXd> const& inputs,
+                                Eigen::VectorXd const& sens) {
+  if (client.SupportsGradient()) {
+    client.Gradient(outWrt, inWrt, inputs, sens, config);
+    gradient = client.gradient;
+  } else
+    gradient = GradientByFD(outWrt, inWrt, inputs, sens);
+}
+
+void HTTPModPiece::ApplyJacobianImpl(unsigned int outWrt,
+                                    unsigned int inWrt,
+                                    muq::Modeling::ref_vector<Eigen::VectorXd> const& inputs,
+                                    Eigen::VectorXd const& vec){
+  if (client.SupportsApplyJacobian()) {
+    client.ApplyJacobian(outWrt, inWrt, inputs, vec, config);
+    jacobianAction = client.jacobianAction;
+  } else
+    jacobianAction = ApplyJacobianByFD(outWrt, inWrt, inputs, vec);
+}
+
+void HTTPModPiece::ApplyHessianImpl(unsigned int outWrt,
+                                    unsigned int inWrt1,
+                                    unsigned int inWrt2,
+                                    muq::Modeling::ref_vector<Eigen::VectorXd> const& inputs,
+                                    Eigen::VectorXd const& sens,
+                                    Eigen::VectorXd const& vec){
+  if (client.SupportsApplyHessian()) {
+    client.ApplyHessian(outWrt, inWrt1, inWrt2, inputs, sens, vec, config);
+    hessAction = client.hessAction;
+  } else
+    hessAction = ApplyHessianByFD(outWrt, inWrt1, inWrt2, inputs, sens, vec);
 }
