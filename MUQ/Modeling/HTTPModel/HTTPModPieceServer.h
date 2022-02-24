df18e4e14655fe4bc60c5168660efc116d2ebcdd
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2022-02-24 18:25:58 +0100

    Add server side support, exposing ModPiece via HTTP

---
+++ MUQ/Modeling/HTTPModel/HTTPModPieceServer.h
@@ -0,0 +1,59 @@
+#include "MUQ/Modeling/HTTPModel/HTTPModPiece.h"
+#include "MUQ/Modeling/HTTPModel/HTTPComm.h"
+
+namespace muq {
+  namespace Modeling {
+    class HTTPModPieceWrapper : public ShallowModPiece {
+    public:
+
+      HTTPModPieceWrapper(std::shared_ptr<muq::Modeling::ModPiece> modPiece)
+      : ShallowModPiece(modPiece->inputSizes, modPiece->outputSizes), modPiece(modPiece)
+      {}
+
+      void Evaluate(std::vector<std::reference_wrapper<const Eigen::VectorXd>> const& inputs, json config) override {
+        outputs = modPiece->Evaluate(inputs);
+      }
+
+      void Gradient(unsigned int outWrt,
+                    unsigned int inWrt,
+                    std::vector<std::reference_wrapper<const Eigen::VectorXd>> const& inputs,
+                    Eigen::VectorXd const& sens,
+                    json config = json()) override {
+        gradient = modPiece->Gradient(outWrt, inWrt, inputs, sens);
+
+      }
+
+      void ApplyJacobian(unsigned int outWrt,
+                    unsigned int inWrt,
+                    std::vector<std::reference_wrapper<const Eigen::VectorXd>> const& inputs,
+                    Eigen::VectorXd const& vec,
+                    json config = json()) override {
+        jacobianAction = modPiece->ApplyJacobian(outWrt, inWrt, inputs, vec);
+      }
+
+      void ApplyHessian(unsigned int outWrt,
+                        unsigned int inWrt1,
+                        unsigned int inWrt2,
+                        muq::Modeling::ref_vector<Eigen::VectorXd> const& inputs,
+                        Eigen::VectorXd const& sens,
+                        Eigen::VectorXd const& vec,
+                        json config = json()) override {
+        hessAction = modPiece->ApplyHessian(outWrt, inWrt1, inWrt2, inputs, sens, vec);
+      }
+
+      bool SupportsEvaluate() override {return true;}
+      bool SupportsGradient() override {return true;} // Expose derivative information as well,
+      bool SupportsApplyJacobian() override {return true;} // since we fall back to finite differences automatically
+      bool SupportsApplyHessian() override {return true;}
+
+    private:
+      std::shared_ptr<muq::Modeling::ModPiece> modPiece;
+    };
+
+    void serveModPiece(std::shared_ptr<ModPiece> modPiece, std::string host, int port) {
+      HTTPModPieceWrapper wrapper(modPiece);
+      ::serveModPiece(wrapper, host, port);
+    }
+
+  }
+}
