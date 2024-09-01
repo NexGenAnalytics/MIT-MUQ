7ebecd3335c7e02b3e7eabfced18d390120de77e
Francesco Rizzi <francesco.rizzi@ng-analytics.com>
2024-09-01 21:21:52 +0200

    update

--- SupportScripts/build_tpls.py
+++ SupportScripts/build_tpls.py
@@ -12,7 +12,6 @@ def remove_everything_if_needed_from(pathdir, force_rebuild: bool):
     if os.path.exists(pathdir) and force_rebuild:
         print(f'removing all content in {pathdir} becuase -f was passed')
         shutil.rmtree(pathdir)
-        
 
 def get_full_path_to_cmake_config_dir(
     install_path_to_search: Union[str, os.PathLike],
@@ -23,14 +22,14 @@ def get_full_path_to_cmake_config_dir(
 
 
 def build_install_boost(
-    workdir: str, 
+    workdir: str,
     force_rebuild: bool
 ):
     # boost is special since we cannot use cmake for this version
-    
+
     myname = "boost"
     parent_path = os.path.join(workdir, myname)
-    
+
     zip_name = "boost_1_85_0.tar.gz"
     url = f'https://archives.boost.io/release/1.85.0/source/{zip_name}'
     zip_path = os.path.join(parent_path, zip_name)
@@ -55,15 +54,15 @@ def build_install_boost(
     shutil.unpack_archive(zip_path, parent_path)
 
     os.chdir(unpack_path)
-    
+
     # configure
     print("2. configuring")
     exeargs = (
         "./bootstrap.sh",
         f'--prefix={install_path}',
-        "--with-libraries=graph")
+        "--with-libraries=system, graph")
     print(exeargs)
-    
+
     logfile = open(parent_path + "/logfile_config", "w")
     p = subprocess.Popen(exeargs, stdout=logfile, stderr=logfile)
     p.wait()
@@ -71,7 +70,7 @@ def build_install_boost(
     assert p.returncode == 0
 
     # make and install
-    print("3. make and install")            
+    print("3. make and install")
     exeargs = ("./b2", "-j4", "install")
     logfile = open(parent_path + "/logfile_makeinstall", "w")
     p = subprocess.Popen(exeargs, stdout=logfile, stderr=logfile)
@@ -80,7 +79,7 @@ def build_install_boost(
     assert p.returncode == 0
 
     print(f'success installing: {myname}\n')
-    
+
     target = 'BoostConfig.cmake'
     return get_full_path_to_cmake_config_dir(install_path, target)
 
@@ -115,16 +114,16 @@ def build_install_impl(
     shutil.unpack_archive(zippath, parentdir)
 
     # configure
-    print("2. configuring")        
+    print("2. configuring")
     exeargs = (
         "cmake",
         "-DCMAKE_CXX_COMPILER=", os.environ['CXX'],
-        "-S", unpacked, 
+        "-S", unpacked,
         "-B", builddir,
         f'-DCMAKE_INSTALL_PREFIX={installdir}')
     exeargs += cmake_extra_args
     print(exeargs)
-    
+
     logfile = open(parentdir + "/logfile_build", "w")
     p = subprocess.Popen(exeargs, stdout=logfile, stderr=logfile)
     p.wait()
@@ -132,7 +131,7 @@ def build_install_impl(
     assert p.returncode == 0
 
     # make and install
-    print("3. make and install")            
+    print("3. make and install")
     os.chdir(builddir)
     exeargs = ("make", "-j4", "install")
     logfile = open(parentdir + "/logfile_makeinstall", "w")
@@ -142,23 +141,23 @@ def build_install_impl(
     assert p.returncode == 0
 
     print(f'success installing: {tplname}\n')
-    
+
 
 def build_install_hdf5(
-    workdir: str, 
+    workdir: str,
     force_rebuild: bool
 ):
-    myname = "hdf5"    
+    myname = "hdf5"
     parent_path = os.path.join(workdir, myname)
-    
+
     zip_name = "hdf5-1_8_19.zip"
-    url = f'https://github.com/HDFGroup/hdf5/archive/refs/tags/{zip_name}'        
+    url = f'https://github.com/HDFGroup/hdf5/archive/refs/tags/{zip_name}'
     zip_path = os.path.join(parent_path, zip_name)
-    unpack_path = os.path.join(parent_path,  "hdf5-hdf5-1_8_19")    
+    unpack_path = os.path.join(parent_path,  "hdf5-hdf5-1_8_19")
     build_path = os.path.join(parent_path, "build")
     install_path = os.path.join(parent_path, "install")
     custom_cmake_args = (f'-DHDF5_BUILD_CPP_LIB=ON',)
-    
+
     build_install_impl(
         myname,
         force_rebuild,
@@ -171,26 +170,26 @@ def build_install_hdf5(
         install_path,
         custom_cmake_args
     )
-    
+
     target = 'hdf5-config.cmake'
     return get_full_path_to_cmake_config_dir(install_path, target)
 
-    
+
 def build_install_nlopt(
-    workdir: str, 
+    workdir: str,
     force_rebuild: bool
 ):
     myname = "nlopt"
     parent_path = os.path.join(workdir, myname)
-    
+
     zip_name = "v2.8.0.zip"
     url = f'https://github.com/stevengj/nlopt/archive/refs/tags/{zip_name}'
     zip_path = os.path.join(parent_path, zip_name)
-    unpack_path = os.path.join(parent_path,  "nlopt-2.8.0")    
+    unpack_path = os.path.join(parent_path,  "nlopt-2.8.0")
     build_path = os.path.join(parent_path, "build")
     install_path = os.path.join(parent_path, "install")
     custom_cmake_args = ()
-    
+
     build_install_impl(
         myname,
         force_rebuild,
@@ -209,26 +208,26 @@ def build_install_nlopt(
 
 
 def build_install_sundials(
-    workdir: str, 
+    workdir: str,
     force_rebuild: bool
 ):
     myname = "sundials"
     parent_path = os.path.join(workdir, myname)
-    
+
     zip_name = "v5.5.0.zip"
     url = f'https://github.com/LLNL/sundials/archive/refs/tags/{zip_name}'
     zip_path = os.path.join(parent_path, zip_name)
-    unpack_path = os.path.join(parent_path,  "sundials-5.5.0")    
+    unpack_path = os.path.join(parent_path,  "sundials-5.5.0")
     build_path = os.path.join(parent_path, "build")
     install_path = os.path.join(parent_path, "install")
     custom_cmake_args = (
-        "-DBUILD_CVODE=OFF", 
+        "-DBUILD_CVODE=OFF",
         "-DBUILD_CVODES=ON",
         "-DBUILD_IDA=OFF",
         "-DBUILD_IDAS=ON",
         "-DBUILD_KINSOL=ON",
     )
-    
+
     build_install_impl(
         myname,
         force_rebuild,
@@ -247,12 +246,12 @@ def build_install_sundials(
 
 
 def build_install_eigen(
-    workdir: str, 
+    workdir: str,
     force_rebuild: bool
 ):
     myname = "eigen"
     parent_path = os.path.join(workdir, myname)
-    
+
     zip_name = "eigen-3.3.7.zip"
     url = f'https://gitlab.com/libeigen/eigen/-/archive/3.3.7/{zip_name}'
     zip_path = os.path.join(parent_path, zip_name)
@@ -260,7 +259,7 @@ def build_install_eigen(
     build_path = os.path.join(parent_path, "build")
     install_path = os.path.join(parent_path, "install")
     custom_cmake_args = ()
-    
+
     build_install_impl(
         myname,
         force_rebuild,
@@ -273,28 +272,28 @@ def build_install_eigen(
         install_path,
         custom_cmake_args
     )
-    
+
     target = 'Eigen3Config.cmake'
     return get_full_path_to_cmake_config_dir(install_path, target)
 
 
 def build_install_nanoflann(
-    workdir: str, 
+    workdir: str,
     force_rebuild: bool
 ):
     myname = "nanoflann"
     parent_path = os.path.join(workdir, myname)
-    
+
     zip_name = "v1.5.5.zip"
-    url = f'https://github.com/jlblancoc/nanoflann/archive/refs/tags/{zip_name}'        
+    url = f'https://github.com/jlblancoc/nanoflann/archive/refs/tags/{zip_name}'
     zip_path = os.path.join(parent_path, zip_name)
-    unpack_path = os.path.join(parent_path,  "nanoflann-1.5.5")    
+    unpack_path = os.path.join(parent_path,  "nanoflann-1.5.5")
     build_path = os.path.join(parent_path, "build")
     install_path = os.path.join(parent_path, "install")
     custom_cmake_args = (
         "-DNANOFLANN_BUILD_EXAMPLES=OFF",
         "-DNANOFLANN_BUILD_TESTS=OFF")
-    
+
     build_install_impl(
         myname,
         force_rebuild,
@@ -307,7 +306,7 @@ def build_install_nanoflann(
         install_path,
         custom_cmake_args
     )
-    
+
     target = 'nanoflannConfig.cmake'
     return get_full_path_to_cmake_config_dir(install_path, target)
 
@@ -320,10 +319,10 @@ def check_compilers():
 
     if not cxx_alias_found:
         print(f'-'*50)
-        print(f'*** FATAL ERROR ***') 
+        print(f'*** FATAL ERROR ***')
         print(f'-'*50)
-        print(f'CXX not found in environment, I cannot proceed!') 
-        print(f'please set CXX to a valid C++ compiler') 
+        print(f'CXX not found in environment, I cannot proceed!')
+        print(f'please set CXX to a valid C++ compiler')
         print(f'-'*50)
         exit(11)
 
@@ -342,14 +341,14 @@ def add_arguments(parser):
     )
 
     parser.add_argument(
-        "-f", 
-        dest="force_rebuild", 
-        required=False, 
+        "-f",
+        dest="force_rebuild",
+        required=False,
         action="store_true",
         help="Use it to delete everything inside the working directory, "
         "and build everything from scratch."
     )
-    
+
     parser.add_argument(
         "--with",
         dest="target_list",
@@ -394,7 +393,7 @@ if __name__ == "__main__":
             paths_for_config['HDF5_DIR'] = [tpl + ' path', p]
 
         if tpl == 'nlopt':
-            p = build_install_nlopt(args.workdir, args.force_rebuild)        
+            p = build_install_nlopt(args.workdir, args.force_rebuild)
             paths_for_config['NLopt_DIR'] = [tpl + ' path', p]
 
         if tpl == 'boost':
@@ -402,11 +401,11 @@ if __name__ == "__main__":
             paths_for_config['Boost_DIR'] = [tpl + ' path', p]
 
         if tpl == 'sundials':
-            p = build_install_sundials(args.workdir, args.force_rebuild)                
+            p = build_install_sundials(args.workdir, args.force_rebuild)
             paths_for_config['SUNDIALS_DIR'] = [tpl + ' path', p]
 
         if tpl == 'eigen':
-            p = build_install_eigen(args.workdir, args.force_rebuild)                
+            p = build_install_eigen(args.workdir, args.force_rebuild)
             paths_for_config['Eigen3_DIR'] = [tpl + ' path', p]
 
         if tpl == 'nanoflann':
@@ -421,4 +420,3 @@ if __name__ == "__main__":
             f.write(f'set({k} {v[1]} CACHE PATH "{v[0]}")\n')
     print(f'-'*50)
     print(f'use "-C {cache_file_out}" to find the selected TPLs when building MUQ')
-
