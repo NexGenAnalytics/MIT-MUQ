337ef56d256ece063ea809c46e27a7c153f90c31
Francesco Rizzi <francesco.rizzi@ng-analytics.com>
2024-09-02 13:30:40 +0200

    remove comments

--- cmake/SetupTestTargets.cmake
+++ cmake/SetupTestTargets.cmake
@@ -1,11 +1,5 @@
 # only build the tests if some of them should be built
 IF(MUQ_USE_GTEST)
-    #set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -DGTEST_USE_OWN_TR1_TUPLE=1")
-    # CHECK_CXX_COMPILER_FLAG("-std=c++11" HAS_PTHREAD)
-    # if(HAS_PTHREAD)
-	# set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -pthread")
-    # endif()
-
     # Add all of the relevant GTEST sources
     set(all_gtest_sources modules/RunTests.cpp)
     set(all_gtest_sources_parallel modules/RunParallelTests.cpp)
