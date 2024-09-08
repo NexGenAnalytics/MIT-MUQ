cfce1ae28e7036331d4155765c987f41e187f00d
Francesco Rizzi <francesco.rizzi@ng-analytics.com>
2024-09-08 17:04:16 +0200

    required

--- cmake/DependencySearch.cmake
+++ cmake/DependencySearch.cmake
@@ -40,8 +40,8 @@ set(MUQ_HAS_SUNDIALS 0) # needed for preprocessor directives in the MUQ source c
 list (FIND MUQ_REQUIRES SUNDIALS dindex)
 if (${dindex} GREATER -1)
     find_package(SUNDIALS 5.5.0...<6.0.0 REQUIRED)
-    set(MUQ_HAS_SUNDIALS 1) 
-    LIST(APPEND MUQ_LINK_LIBS 
+    set(MUQ_HAS_SUNDIALS 1)
+    LIST(APPEND MUQ_LINK_LIBS
         SUNDIALS::cvodes  SUNDIALS::idas SUNDIALS::kinsol SUNDIALS::nvecserial)
 endif()
 
@@ -74,8 +74,8 @@ endif()
 list (FIND MUQ_REQUIRES BOOST dindex)
 if (${dindex} GREATER -1)
     set(BOOST_MIN_VERSION "1.56.0")
-    find_package(Boost ${BOOST_MIN_VERSION} COMPONENTS system filesystem graph regex)
-    LIST(APPEND MUQ_LINK_LIBS 
+    find_package(Boost ${BOOST_MIN_VERSION} REQUIRED COMPONENTS system filesystem graph regex)
+    LIST(APPEND MUQ_LINK_LIBS
         Boost::system Boost::filesystem Boost::graph Boost::regex)
 endif()
 
@@ -83,8 +83,8 @@ endif()
 # ##### LOOK FOR Parallel Sampling Algorithm dependencies  ######
 # ###############################################################
 
-set(MUQ_HAS_PARCER 0) # needed for preprocessor directives 
-set(MUQ_HAS_OTF2 0)   # needed for preprocessor directives 
+set(MUQ_HAS_PARCER 0) # needed for preprocessor directives
+set(MUQ_HAS_OTF2 0)   # needed for preprocessor directives
 set(MUQ_HAS_MPI 0)
 if(MUQ_USE_MPI)
     # PARCER
@@ -116,9 +116,9 @@ if(MUQ_USE_MPI)
 endif()
 
 
-# REMOVE DUPLICATES 
+# REMOVE DUPLICATES
 list( REMOVE_DUPLICATES MUQ_EXTERNAL_INCLUDES)
-set(MUQ_EXTERNAL_INCLUDE_DIRS ${MUQ_EXTERNAL_INCLUDES} 
+set(MUQ_EXTERNAL_INCLUDE_DIRS ${MUQ_EXTERNAL_INCLUDES}
     CACHE INTERNAL "List of external include directories for MUQ.")
 
 
@@ -222,4 +222,3 @@ endif()
 if(MUQ_LINK_LIBS)
   list( REMOVE_DUPLICATES MUQ_LINK_LIBS)
 endif()
-
