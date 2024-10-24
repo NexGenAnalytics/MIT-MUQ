814e5b3fe41abe577b726a1a6b453255dffb469c
Francesco Rizzi <francesco.rizzi@ng-analytics.com>
2024-10-24 15:11:42 -0400

    update

--- BuildAllExamples.sh
+++ BuildAllExamples.sh
@@ -80,8 +80,7 @@ for it in "${ARRAY[@]}" ; do
     # Simply add exit codes; will be zero if all tests successful
     exit_code=$(($exit_code + $make_exit)) 
 
-    # in some cases there might be that the executable is not present, 
-    # for example for MPI tests like ParallelMultilevelMonteCarlo
+    # in some cases there might be that the executable is not present
     if [ -f ${exampleBuildDir}/${executableName} ]; then
         if [ "$executableName" != "void" ]; then
             cd ${exampleBuildDir}
@@ -94,7 +93,6 @@ for it in "${ARRAY[@]}" ; do
 done
 
 echo -e $summary
-
 echo ""
 
 if [ $exit_code -eq 0 ]
@@ -105,140 +103,3 @@ else
 fi
 
 exit $exit_code
-
-
-
-
-
-
-
-
-
-
-# summary=""
-
-# directories=(
-# # "Approximation/GaussianProcess_CO2"
-# # "Approximation/MonotoneRegression"
-
-# # "Modeling/FlowEquation/cpp"
-# # "Modeling/UMBridge"
-# # "Modeling/CustomModPiece/cpp"
-# # "Modeling/MemoryTest/cpp"
-# # "Modeling/WorkGraphs/cpp"
-
-# # "SamplingAlgorithms/MC/Example1_Gaussian"
-
-# # "SamplingAlgorithms/MCMC/BasicMetropolisInGibbs/cpp"
-# # "SamplingAlgorithms/MCMC/CustomGaussianProposal/cpp"
-# # "SamplingAlgorithms/MCMC/EllipticInference/cpp"
-# # "SamplingAlgorithms/MCMC/MultilevelMCMC_FlowModel/cpp"
-# # "SamplingAlgorithms/MCMC/Example1_Gaussian/cpp"
-# # "SamplingAlgorithms/MCMC/Example2_GaussianInverseGamma/cpp"
-# # "SamplingAlgorithms/MCMC/Example3_MultilevelGaussian/cpp"
-# # "SamplingAlgorithms/MCMC/Example4_MultiindexGaussian/cpp"
-# # "SamplingAlgorithms/MCMC/Example5_MALASampling"
-
-# "Utilities/HDF5/BlockOperations"
-# "Utilities/HDF5/SimpleReadWrite"
-# )
-
-# binaries=(
-#     # "Modeling/FlowEquation/cpp/build/FlowEquation"
-#     # "Modeling/CustomModPiece/cpp/build/BasicModPiece"
-#     # "Modeling/MemoryTest/cpp/build/MemoryUsage"
-#     # "Modeling/WorkGraphs/cpp/build/SimpleWorkGraph"
-#     # "Modeling/WorkGraphs/cpp/build/SplitSum"
-
-#     # "SamplingAlgorithms/MC/Example1_Gaussian/build/MonteCarlo"
-#     # "SamplingAlgorithms/MC/Example1_Gaussian/build/MultilevelMonteCarlo"
-
-#     # "SamplingAlgorithms/MCMC/BasicMetropolisInGibbs/cpp/build/MetropolisInGibbs"
-#     # "SamplingAlgorithms/MCMC/CustomGaussianProposal/cpp/build/CustomProposal"
-#     # "SamplingAlgorithms/MCMC/EllipticInference/cpp/build/InvariantSampling"
-#     # "SamplingAlgorithms/MCMC/Example1_Gaussian/cpp/build/GaussianSampling"
-#     # "SamplingAlgorithms/MCMC/Example2_GaussianInverseGamma/cpp/build/GaussianGammaSampling"
-#     # "SamplingAlgorithms/MCMC/Example3_MultilevelGaussian/cpp/build/BasicMultilevel"
-#     # "SamplingAlgorithms/MCMC/Example3_MultilevelGaussian/cpp/build/AdvancedMultilevel"
-#     # "SamplingAlgorithms/MCMC/Example4_MultiindexGaussian/cpp/build/MultiindexGaussianSampling"
-#     # "./SamplingAlgorithms/MCMC/Example5_MALASampling/build/malaSampling"
-
-#     "Utilities/HDF5/BlockOperations/build/BlockOperations"
-#     "Utilities/HDF5/SimpleReadWrite/build/SimpleReadWrite"
-# )
-
-# if [[ $flag == 1 ]]; then 
-
-#     for dir in "${directories[@]}"
-#     do
-#         echo "======================"
-#         echo "Building example in examples/$dir"
-#         echo ""
-
-#         mkdir -p examples/$dir/build
-#         rm -rf examples/$dir/build/*
-#         cmake -S examples/$dir -B examples/$dir/build
-#         cmake_exit=$?
-#         if [ $cmake_exit -eq 0 ]
-#         then
-#             echo "CMake successful for example examples/$dir"
-#         else
-#             echo "CMake failed for example examples/$dir"
-#             summary="$summary\nCMake FAILED for example examples/$dir"
-#         fi
-#         # Simply add exit codes; will be zero if all tests successful
-#         exit_code=$(($exit_code + $cmake_exit)) 
-
-#         make -C examples/$dir/build
-#         make_exit=$?
-#         if [ $make_exit -eq 0 ]
-#         then
-#             echo "Make successful for example examples/$dir"
-#             summary="$summary\nBuild successful for example examples/$dir"
-#         else
-#             echo "Make failed for example examples/$dir"
-#             summary="$summary\nMake FAILED for example examples/$dir"
-#         fi
-#         # Simply add exit codes; will be zero if all tests successful
-#         exit_code=$(($exit_code + $make_exit)) 
-
-#     done
-#     echo "======================"
-# fi
-
-# if [[ $flag == 2 ]]; then 
-
-#     for bin in "${binaries[@]}"
-#     do
-#         echo "======================"
-#         echo "Running example $bin"
-#         echo ""
-
-#         examples/$bin
-#         run_exit=$?
-#         if [ $run_exit -eq 0 ]
-#         then
-#             echo "Run successful for example $bin"
-#             summary="$summary\nRun successful for example $bin"
-#         else
-#             echo "Run failed for example $bin"
-#             summary="$summary\nRun FAILED for example $bin"
-#         fi
-#         exit_code=$(($exit_code + $run_exit)) # Simply add exit codes; will be zero if all tests successful
-#     done
-#     echo "======================"
-
-# fi
-
-# echo -e $summary
-
-# echo ""
-
-# if [ $exit_code -eq 0 ]
-# then
-#     echo "All examples successful."
-# else
-#     echo "Some examples FAILED!"
-# fi
-
-# exit $exit_code
